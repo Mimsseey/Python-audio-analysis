0190891272b5ed45093934c82d3a906cc76a1ba7
Mimsseey <56138496+Mimsseey@users.noreply.github.com>
2021-05-29 21:19:12 +0200

    Update mimi_spectro.py

--- mimi_spectro.py
+++ mimi_spectro.py
@@ -1,29 +1,25 @@
- import os
- os.getcwd()
-'C:\\Windows\\system32'
- os.chdir(r"C:\Users\Miha\Desktop\py")
- import librosa
- import librosa.display
- import numpy as np
- import matplotlib.pyplot as plt
- import IPython.display as ipd
- mimi_wav = "mimi.wav"
- y, sr = librosa.load(mimi_wav, sr=None, mono=True)
- print(type(y), type(sr))
- class 'numpy.ndarray'> <class 'int'>
- print(y.shape, sr)
-(340609,) 48000
- plt.figure(figsize=(14, 5))
- Figure size 1400x500 with 0 Axes>
- librosa.display.waveplot(y, sr=sr)
- plt.show()
- n_fft = 2048
- D = np.abs(librosa.stft(y[:n_fft], n_fft=n_fft, hop_length=n_fft+1))
- plt.plot(D);
- plt.show()
- plt.colorbar();
- plt.show()
- DB = librosa.amplitude_to_db(D, ref=np.max)
- librosa.display.specshow(DB, sr=sr, hop_length=hop_length, x_axis='time', y_axis='log');
- plt.colorbar(format='%+2.0f dB');
- plt.show()
+#**This code is a mix of examples from [blog] (https://towardsdatascience.com/extract-features-of-music-75a3f9bc265d) & [Librosa package site] (https://librosa.org/)**#
+
+
+import os
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+#change directory to where wav files are located
+os.chdir(r"C:\Users\XXXX\")
+
+mimi_mjau = "TEST_mimi.wav" #import wav file
+y, sr = librosa.load(mimi_mjau,) 
+plt.figure(figsize=(15, 6))
+librosa.display.waveplot(y, sr=sr)
+plt.show()
+n_fft = 2048
+D = np.abs(librosa.stft(y[:n_fft], n_fft=n_fft, hop_length=n_fft+1))
+plt.plot(D)
+plt.show()
+DB = librosa.amplitude_to_db(D, ref=np.max)
+librosa.display.specshow(DB, sr=sr, x_axis='time', y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.show()
